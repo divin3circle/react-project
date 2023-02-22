14933d740b0e631534a43814909e31a1f222c514
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-02-23 00:49:42 +0300

    reaction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
+import Todo from './components/ToDo';
+
 function App() {
-  return ( 
-  <div>
-    <h1>My To Do</h1>
-    <div className='card'>
-      <h2>TITLE</h2>
-      <div className='actions'>
-        <button className='btn'>Remove To Do</button>      
-      </div>
+  return (
+    <div>
+      <h1>My To Do</h1>
+      <Todo />
     </div>
-  </div>
   );
 }
 
 export default App;
-
---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+function Todo() {
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className="actions">
+        <button className="btn">Remove To Do</button>
+      </div>
+    </div>
+  );
+}
+export default Todo;
