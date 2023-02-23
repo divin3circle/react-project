d19f97a64719e01173312b264280c708ca56e455
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-02-23 19:12:08 +0300

    reaction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Todo from './components/ToDo';
 
+
 function App() {
   return (
     <div>
       <h1>My To Do</h1>
-      <Todo />
+      <Todo text='Introduction'/>
+      <Todo text='Intermediate react' />
+      <Todo text='God-mode react' />
+      <Todo text='Teach react' />
+
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+  return (
+    <div className='modal'>
+      <p>Please confirm delete</p>
+      <button className='btn' >Confirm</button>
+      <button className='btn btn--alt'>Cancel</button>
+    </div>
+  );
+}
+
+export default Modal;
