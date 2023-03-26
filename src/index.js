4c5c23c6775435d5981150f2be6d5d7d0fa5521b
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-03-26 15:11:27 +0300

    first-app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom";
+import React from "react";
+import "./index.css";
 
-//starting point
-ReactDOM.render(<App />, document.getElementById('root'));
+function Greetings({ name }) {
+  /*   const [name] = props; */
+  return (
+    <div id="greet">
+      <h1>Hello,</h1>
+      <p>I'm {name}</p>
+    </div>
+  );
+}
 
+function Add() {
+  return (
+    <section className="add">
+      <h4>What can I do for you?</h4>
+      <button
+        onClick={() => {
+          console.log("You reloded the page");
+        }}
+      >
+        Reload the page
+      </button>
+      <button
+        onClick={() => {
+          alert("Some qoute");
+        }}
+      >
+        Display a qoute
+      </button>
+    </section>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Greetings name="Sylus Abel" />
+      <Add />
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
